41937a547e52c96da6e2429496ed42959725cd01
MehdiLamatig <77162581+MehdiLamatig@users.noreply.github.com>
2021-03-12 12:14:30 +0100

    up

--- TP2/Exo 3 html/index.php
+++ TP2/Exo 3 html/index.php
@@ -1,24 +1,23 @@
-<doctype html>
-    <html>
-      <?php
-      require_once('template_header.php');
-      ?>
-      <body>
-        <h1>Qui suis-je ?</h1>
-        <?php
-        require_once('template_menu.php');
-        renderMenuToHTML('index');
-        ?>
-        <article>
-          <h2 style="color:purple;">Etudiant-ingénieur à la recherche d'un stage de 4 mois durant lequel je serai amené à:</h2>
-          <section style="color:cadetblue;">Co-piloter un ou plusieurs projets</section>
-          <section style="color:crimson;">Gérer, synthétiser, améliorer et communiquer sur des indicateurs d'activité et/ou de performance</section>
-          <section style="color:mediumorchid">Participer au développement de solutions technologiques ou managériales</section>
-          <section style="color:peru">Accompagner les équipes à l'intégration du changement et l'appropriation de nouveaux process</section>
-          <footer>Ce stage représente ainsi pour moi l’occasion de développer à la fois ma compréhension des enjeux stratégiques de l'entreprise mais aussi de mieux appréhender la contribution des ingénieurs à la performance et au développement de celle-ci.</footer>
-        </article>
-        <?php
-        require_once('template_footer.php');
-        ?>
-      </body>
-    </html>
+<?php
+require_once('template_header.php');
+?>
+
+<h1>Qui suis-je ?</h1>
+
+<?php
+require_once('template_menu.php');
+renderMenuToHTML('index');
+?>
+
+<article class="corps">
+  <h2 style="color:purple;">Etudiant-ingénieur à la recherche d'un stage de 4 mois durant lequel je serai amené à:</h2>
+  <section style="color:cadetblue;">Co-piloter un ou plusieurs projets</section>
+  <section style="color:crimson;">Gérer, synthétiser, améliorer et communiquer sur des indicateurs d'activité et/ou de performance</section>
+  <section style="color:mediumorchid">Participer au développement de solutions technologiques ou managériales</section>
+  <section style="color:peru">Accompagner les équipes à l'intégration du changement et l'appropriation de nouveaux process</section>
+  <footer>Ce stage représente ainsi pour moi l’occasion de développer à la fois ma compréhension des enjeux stratégiques de l'entreprise mais aussi de mieux appréhender la contribution des ingénieurs à la performance et au développement de celle-ci.</footer>
+</article>
+
+<?php
+require_once('template_footer.php');
+?>
