ed9be87f35d3aa461d3138cf0da820e894559ef6
MehdiLamatig <77162581+MehdiLamatig@users.noreply.github.com>
2021-03-15 11:27:20 +0100

    factorisation

--- TP2/Exo 6/index.php
+++ TP2/Exo 6/index.php
@@ -9,14 +9,14 @@ if(isset($_GET['page'])) {
 ?>
 
 <header class="bandeau_haut">
-<h1 class="titre">Qui suis-je ?</h1>
+<h1 class="titre">Mehdi Lamatig</h1>
 </header>
 
 <?php
 renderMenuToHTML($currentPageId);
 ?>
 
-<article class="corps">
+<section class="corps">
 <?php
 $pageToInclude = $currentPageId . ".php";
 if(is_readable($pageToInclude))
@@ -24,13 +24,7 @@ require_once($pageToInclude);
 else
 require_once("error.php");
 ?>
-<h2 style="color:purple;">Etudiant-ingénieur à la recherche d'un stage de 4 mois durant lequel je serai amené à:</h2>
-  <section style="color:cadetblue;">Co-piloter un ou plusieurs projets</section>
-  <section style="color:crimson;">Gérer, synthétiser, améliorer et communiquer sur des indicateurs d'activité et/ou de performance</section>
-  <section style="color:mediumorchid">Participer au développement de solutions technologiques ou managériales</section>
-  <section style="color:peru">Accompagner les équipes à l'intégration du changement et l'appropriation de nouveaux process</section>
-  <footer>Ce stage représente ainsi pour moi l’occasion de développer à la fois ma compréhension des enjeux stratégiques de l'entreprise mais aussi de mieux appréhender la contribution des ingénieurs à la performance et au développement de celle-ci.</footer>
-</article>
+</section>
 
 <?php
 require_once('template_footer.php');
