8ecdd69cac96ac5624c16efbeb78f4be75778cef
MehdiLamatig <77162581+MehdiLamatig@users.noreply.github.com>
2021-03-15 11:18:56 +0100

    6factorisation

---
+++ TP2/Exo 6/cv.php
@@ -0,0 +1,20 @@
+<?php
+require_once('template_header.php');
+?>
+
+<h1>Mon CV</h1>
+
+<?php
+require_once('template_menu.php');
+renderMenuToHTML('cv');
+?>
+
+<article class="corps">
+    <section>Formation</section>
+    <section>Expérience</section>
+    <section><a href="CVMehdiLamatig.pdf">Mon CV</a></section>
+</article>
+
+<?php
+require_once('template_footer.php');
+?>
