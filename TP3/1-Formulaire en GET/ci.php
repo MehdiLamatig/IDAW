4497078de734c2d6a8eeff8025883c3acc04ebc1
MehdiLamatig <77162581+MehdiLamatig@users.noreply.github.com>
2021-03-15 17:27:05 +0100

    tp3

---
+++ TP3/1-Formulaire en GET/ci.php
@@ -0,0 +1,7 @@
+<h1>Centre d'intérêts</h1>
+
+<article class="corps">
+    <section>Sport</section>
+    <section>Management</section>
+    <section>Voyage</section>
+</article>
---
+++ TP3/1-Formulaire en GET/template_footer.php
@@ -0,0 +1,5 @@
+        <footer>
+            N'hésitez pas à consulter mon CV ou me contacter si vous voulez en savoir plus.
+        </footer>
+    </body>
+</html>
