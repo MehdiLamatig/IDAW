b6c9ae501ffade06b9aec2ca93ba30c2b4623c6b
MehdiLamatig <77162581+MehdiLamatig@users.noreply.github.com>
2021-03-19 10:12:02 +0100

    1.3

--- TP3/3-Gestion de cookies/index.php
+++ TP3/3-Gestion de cookies/index.php
@@ -1,25 +1,15 @@
-<form id="style_form" action="index.php" method="POST">
-    <select name="css">
-        <option value="style1">style1</option>
-        <option value="style2">style2</option>
-    </select>
-    <input type="submit" value="Appliquer" />
-</form>
+<?php
+    $style = 'style1';
+    if (isset($_COOKIE['style']))
+        $style = $_COOKIE['style'];
+    if(isset($_POST['css'])){
+        $style = $_POST['css'];
+        setcookie('style',$style);
+    }
+?>
 
 <!doctype html>
 <html>
-
-<?php
-    if(isset($_POST['css'])){
-        setcookie('style',$_POST['css'], time()+3600);
-    }
-    if (isset($_COOKIE['style'])){
-        $style = $_COOKIE['style'];
-    }
-    else
-        $style = 'style1';
-    print($style)
-?>
     <head>
         <title>Mehdi Lamatig</title>
         <?php
@@ -27,4 +17,13 @@
         media=\"screen\" title=\"default\" charset=\"utf-8\" />";
         ?>
     </head>
+    <body>
+        <form id="style_form" action="index.php" method="POST">
+            <select name="css">
+                <option value="style1">style1</option>
+                <option value="style2">style2</option>
+            </select>
+            <input type="submit" value="Appliquer" />
+        </form>
+    </body>
 </html>
